ee47c00685c690d84b6c40c3aeafb003fd7e2437
minn0004 <thibault.minneboo@etudiant.univ-reims.fr>
2024-03-05 08:37:14 +0100

    feat: add default page for home (to edit in the future like 404 page)

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,7 +4,7 @@ export default function Routes() {
     return (
         <Switch>
 
-            <!-- <Route path="/" component={HomeComponent} /> -->
+            <Route path="/" component={() => <h1 className="text-red-500">Home</h1>}/>
 
             /* Default | 404 Not found */
             <Route component={() => <h1>404 Not Found</h1>} />
