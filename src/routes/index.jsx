58078254d6bafd6a0de3230fbd145a9e8f2cb4b6
minn0004 <thibault.minneboo@etudiant.univ-reims.fr>
2024-03-05 08:25:57 +0100

    feat: add indication to insert first route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,6 +4,8 @@ export default function Routes() {
     return (
         <Switch>
 
+            <!-- <Route path="/" component={HomeComponent} /> -->
+
             /* Default | 404 Not found */
             <Route component={() => <h1>404 Not Found</h1>} />
         </Switch>
