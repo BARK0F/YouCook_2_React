9e09506f79e72d90b732491e1ff298e668dc55f2
Nassim MEKHALDI <nassim.mekhaldi@etudiant.univ-reims.fr>
2024-03-12 10:29:11 +0100

    add: ajout d'un composant RecipeCard.jsx pour afficher une carte de la recette

---
+++ src/components/RecipeCard.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function RecipeCard({ recipe }) {
+  return (
+    <div className="bg-white rounded-lg shadow-md p-4 max-w-sm m-4">
+      <h5 className="text-lg font-bold mb-2">
+        {recipe.name ?? "Nom de recette non existant"}
+      </h5>
+      <div className="flex-col">
+        <div>
+          auteur: {recipe?.author ? recipe.author : "Auteur non existant"}
+        </div>
+        <div>note: {recipe?.mark ? recipe.mark : "Aucune Note"}</div>
+      </div>
+    </div>
+  );
+}
+
+RecipeCard.propTypes = {
+  recipe: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    difficulty: PropTypes.string,
+    description: PropTypes.string,
+    nbPeople: PropTypes.number,
+    nbDay: PropTypes.number,
+    nbHour: PropTypes.number,
+    nbMinute: PropTypes.number,
+    mark: PropTypes.number,
+    author: PropTypes.shape(),
+  }),
+};
+
+RecipeCard.defaultProps = {
+  recipe: [],
+};
