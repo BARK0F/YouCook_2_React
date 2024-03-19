093366fc1bc9ce374d0f8176fdead4640d68be72
minn0004 <thibault.minneboo@etudiant.univ-reims.fr>
2024-03-19 10:13:52 +0100

    chore: add navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Routes from "./routes/index.jsx";
+import Navbar from "./components/layout/Navbar.jsx";
 
 function App() {
   return (
     <main className="container">
+      <Navbar />
       <Routes />
     </main>
   );
