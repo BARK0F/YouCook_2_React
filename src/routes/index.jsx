7c8baf1f808f69fee673a9d15085703806bccc93
minn0004 <thibault.minneboo@etudiant.univ-reims.fr>
2024-03-26 17:00:08 +0100

    feat: edit to change color of Home H1

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,7 +6,7 @@ export default function Routes() {
       {/* eslint-disable-next-line react/react-in-jsx-scope */}
       <Route
         path="/"
-        component={() => <h1 className="text-red-500">Home</h1>}
+        component={() => <h1>Home</h1>}
       />
       /* Default | 404 Not found */
       <Route component={() => <h1>404 Not Found</h1>} />
