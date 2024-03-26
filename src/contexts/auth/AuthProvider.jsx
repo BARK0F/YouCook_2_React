4621077d535515dff87aa56333c7f695fde6836b
minn0004 <thibault.minneboo@etudiant.univ-reims.fr>
2024-03-26 17:14:22 +0100

    fix: add catch clause

--- src/contexts/auth/AuthProvider.jsx
+++ src/contexts/auth/AuthProvider.jsx
@@ -10,7 +10,7 @@ export default function AuthProvider({ children }) {
       .then((data) => {
         setUser(data);
       })
-      .then(() => {
+      .catch(() => {
         setUser(null);
       })
   }, []);
