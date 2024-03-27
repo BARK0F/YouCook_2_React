c4212b512ab24115c1f02d6b33d905b6ddd8d300
Dylan Huckel <dylan.huckel@etudiant.univ-reims.fr>
2024-03-27 17:35:43 +0100

    ajout de la fonction fetchAllIngredients

--- src/services/api/ingredient.js
+++ src/services/api/ingredient.js
@@ -1,1 +1,5 @@
 export const BASE_URL = "https://localhost:8000/api/docs/";
+
+export function fetchAllIngredients() {
+  return fetch(`${BASE_URL}ingredients`).then((response) => response.json());
+}
