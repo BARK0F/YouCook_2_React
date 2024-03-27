37bd3c25c2c2accbd3636b88ba77cb61ff3bbdba
Dylan Huckel <dylan.huckel@etudiant.univ-reims.fr>
2024-03-27 16:06:40 +0100

    ajout du composant ingredientItem

--- src/components/IngredientItem.jsx
+++ src/components/IngredientItem.jsx
@@ -1,2 +1,17 @@
-export default function IngredientItem(){
+import PropTypes from "prop-types";
+import Badge from "./Badge.jsx";
+
+export default function IngredientItem({ ingredientName, children }) {
+  return (
+    <Badge children={<span>{ingredientName}</span>}></Badge>
+
+
+  );
 }
+IngredientItem.propTypes = {
+  ingredientName: PropTypes.string,
+};
+
+IngredientItem.defaultProps = {
+  ingredientName: "default",
+};
