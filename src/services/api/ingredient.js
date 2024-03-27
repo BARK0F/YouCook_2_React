dc0ff1eaacf8927f9560fa24f727107cbfedc186
Dylan Huckel <dylan.huckel@etudiant.univ-reims.fr>
2024-03-27 17:36:38 +0100

    ajout de la fonction getIngredientDetail

--- src/services/api/ingredient.js
+++ src/services/api/ingredient.js
@@ -3,3 +3,9 @@ export const BASE_URL = "https://localhost:8000/api/docs/";
 export function fetchAllIngredients() {
   return fetch(`${BASE_URL}ingredients`).then((response) => response.json());
 }
+
+export function getIngredientDetail(id) {
+  return fetch(`${BASE_URL}ingredients/${id}`).then((response) =>
+    response.json(),
+  );
+}
