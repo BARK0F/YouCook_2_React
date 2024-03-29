a6aff77c6e40247b352b8798096214ca284315ef
Nassim MEKHALDI <nassim.mekhaldi@etudiant.univ-reims.fr>
2024-03-29 06:09:52 +0100

    add: utilisation de l'url de base dans les requêtes ajax

--- src/services/recipes.js
+++ src/services/recipes.js
@@ -1,7 +1,5 @@
 export const BASE_URL = "http://127.0.0.1:8000/api";
 
 export function fetchAllRecipes() {
-  return fetch("http://127.0.0.1:8000/api/recipes/").then((response) =>
-    response.json(),
-  );
+  return fetch(`${BASE_URL}/recipes/`).then((response) => response.json());
 }
