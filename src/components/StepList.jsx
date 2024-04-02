c97843e43a5b92d8f0101f52aceb45ac9cf11c1c
Dylan Bonnevie <dylan.bonnevie@etudiant.univ-reims.fr>
2024-04-02 16:22:46 +0200

    Ajout du composant StepList

---
+++ src/components/StepList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Step from "./Step";
+
+export default function StepList({
+    StepsData = null,
+}) {
+    const steps = StepsData?.map((step) => (
+        <Step key={step.id} data={step} />
+    ));
+
+    return (
+        <section className="steps">{steps}</section>
+    );
+}
+
