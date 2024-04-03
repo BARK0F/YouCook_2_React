08cf14c5971b9e0183acc503ca3ec9c330844047
Nassim MEKHALDI <nassim.mekhaldi@etudiant.univ-reims.fr>
2024-04-03 13:53:46 +0200

    fix: correction de la route pour la BASE_URL des requêtes

--- src/services/recipes.js
+++ src/services/recipes.js
@@ -1,7 +1,9 @@
-export const BASE_URL = "http://127.0.0.0:8000/api";
+export const BASE_URL = "http://localhost:8000/api";
 
 export function fetchAllRecipes() {
-  return fetch(`${BASE_URL}/recipes/`).then((response) => response.json());
+  return fetch(`${BASE_URL}/recipes/`, { credentials: "include" }).then(
+    (response) => response.json(),
+  );
 }
 export function fetchRecipe(recipe) {
   return fetch(`${BASE_URL}/recipes/${recipe}`).then((response) =>
