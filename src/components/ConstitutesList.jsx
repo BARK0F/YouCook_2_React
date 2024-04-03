4daeadc0339f3109ad9c90665dc48bc2d9bed4c8
Nassim MEKHALDI <nassim.mekhaldi@etudiant.univ-reims.fr>
2024-04-03 13:56:39 +0200

    add: ajout d'un composant ConstituteList pour afficher la liste des ingrédients d'une recette

---
+++ src/components/ConstitutesList.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ConstituteItem from "./ConstituteItem";
+
+export default function ConstitutesList({ constitutes }) {
+  return (
+    <div className="flex flex-wrap justify-between">
+      {constitutes.map((constitute) => (
+        <ConstituteItem
+          key={constitute["@id"][constitute["@id"].length - 1]}
+          constitute={constitute}
+        />
+      ))}
+    </div>
+  );
+}
+
+ConstitutesList.propTypes = {
+  constitutes: PropTypes.shape({
+    constitute: PropTypes.shape(),
+    map: PropTypes.shape(),
+  }),
+};
+
+ConstitutesList.defaultProps = {
+  constitutes: [],
+};
