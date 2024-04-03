6a366228a87ee9f382be7172b423aa9702999b30
Nassim MEKHALDI <nassim.mekhaldi@etudiant.univ-reims.fr>
2024-04-03 14:49:20 +0200

    fix: correction du style du composant Badge

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -3,16 +3,16 @@ import PropTypes from "prop-types";
 
 export default function Badge({ children }) {
   return (
-    <span className="badge px-3 py-2 mr-4 font-bold border-2 rounded-e">
+    <span className="badge px-3 py-2 mr-4 font-bold border-2 rounded-e bg-white">
       {children}
     </span>
   );
 }
 
 Badge.propTypes = {
-  children: PropTypes.shape(),
+  children: PropTypes.oneOfType([string, number, array]),
 };
 
 Badge.defaultProps = {
-  children: null,
+  children: [],
 };
