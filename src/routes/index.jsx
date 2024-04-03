7eb262a78291ca289ea0c5a0371b33fb6db79431
Nassim MEKHALDI <nassim.mekhaldi@etudiant.univ-reims.fr>
2024-04-03 14:49:44 +0200

    fix: correction du style du titre home

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,12 @@
 import { Route, Switch } from "wouter";
+import React from "react";
 
 export default function Routes() {
   return (
     <Switch>
-      {/* eslint-disable-next-line react/react-in-jsx-scope */}
       <Route
         path="/"
-        component={() => <h1>Home</h1>}
+        component={() => <h1 className="text-red-500">Home</h1>}
       />
       /* Default | 404 Not found */
       <Route component={() => <h1>404 Not Found</h1>} />
