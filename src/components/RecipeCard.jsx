1947e4c642c94ebe456b00a5eb25e26a2252723e
Nassim MEKHALDI <nassim.mekhaldi@etudiant.univ-reims.fr>
2024-04-03 14:19:25 +0200

    add: affichage de la note de la recette dans la carte

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Badge from "./Badge";
 
 export default function RecipeCard({ recipe }) {
+  let sum = 0;
+  recipe.marks.map((mark) => {
+    sum += mark.mark;
+  });
+  sum /= recipe.marks.length;
   return (
     <div className="bg-white rounded-lg shadow-md p-4 max-w-sm m-4">
       <a href={recipe.id}>
         <h5 className="text-lg font-bold mb-2">
           {recipe.name ?? "Nom de recette non existant"}
         </h5>
-        <div className="flex-col">
-          <div>
-            auteur: {recipe?.author ? recipe.author : "Auteur non existant"}
-          </div>
-          <div>note: {recipe?.mark ? recipe.mark : "Aucune Note"}</div>
         <div className="flex flex-row content">
           <Badge className="float-right">
             Auteur : {recipe.author.firstname} {recipe.author.lastname}
+          </Badge>
+          <Badge className="float-left">
+            Note : {!isNaN(sum) ? sum.toFixed(1) : "X.X"}
           </Badge>
         </div>
       </a>
@@ -33,7 +37,7 @@ RecipeCard.propTypes = {
     nbDay: PropTypes.number,
     nbHour: PropTypes.number,
     nbMinute: PropTypes.number,
-    mark: PropTypes.number,
+    marks: PropTypes.shape(),
     author: PropTypes.shape(),
   }),
 };
