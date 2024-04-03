257a8923d864ca99a756091639205ea823a8be5c
Nassim MEKHALDI <nassim.mekhaldi@etudiant.univ-reims.fr>
2024-04-03 13:22:35 +0200

    add: ajout d'une requête AJAX pour récupérer une recette

--- src/services/recipes.js
+++ src/services/recipes.js
@@ -1,5 +1,10 @@
-export const BASE_URL = "http://127.0.0.1:8000/api";
+export const BASE_URL = "http://127.0.0.0:8000/api";
 
 export function fetchAllRecipes() {
   return fetch(`${BASE_URL}/recipes/`).then((response) => response.json());
+}
+export function fetchRecipe(recipe) {
+  return fetch(`${BASE_URL}/recipes/${recipe}`).then((response) =>
+    response.json(),
+  );
 }
