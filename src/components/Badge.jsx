a563fabbb94361445be1c59da0fb4f023135b452
Nassim MEKHALDI <nassim.mekhaldi@etudiant.univ-reims.fr>
2024-04-03 14:17:35 +0200

    fix: correction du style du composant Badge

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 export default function Badge({ children }) {
   return (
-    <span className="badge px-3 py-2 mr-4 font-bold border-2 rounded-e bg-white">
+    <span className="flex flex-wrap w-fit badge px-3 py-2 mr-4 font-bold border-2 rounded-e bg-white">
       {children}
     </span>
   );
