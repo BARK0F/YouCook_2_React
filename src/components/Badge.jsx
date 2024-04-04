e90414d11ad4ba423f6a83a99afdfb1b84b4e5ec
Nassim MEKHALDI <nassim.mekhaldi@etudiant.univ-reims.fr>
2024-04-04 11:58:52 +0200

    fix: correction de l'affichage du composant Badge

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -3,7 +3,7 @@ import PropTypes, { array, number, string } from "prop-types";
 
 export default function Badge({ children }) {
   return (
-    <span className="flex flex-wrap w-fit badge px-3 py-2 mr-4 font-bold border-2 rounded-e bg-white">
+    <span className="flex flex-wrap w-fit h-fit badge px-3 py-2 mr-4 font-bold border-2 rounded-[0.5rem] bg-white">
       {children}
     </span>
   );
