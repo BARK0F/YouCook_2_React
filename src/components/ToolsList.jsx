6842b7218de420b159dcab9e6c37abf836429aa4
Nassim MEKHALDI <nassim.mekhaldi@etudiant.univ-reims.fr>
2024-04-04 13:54:01 +0200

    fix: ajout d'un texte alternatif si aucun outil n'est utilisé

--- src/components/ToolsList.jsx
+++ src/components/ToolsList.jsx
@@ -3,6 +3,9 @@ import PropTypes from "prop-types";
 import Badge from "./Badge";
 
 export default function ToolsList({ tools }) {
+  if (tools.length === 0) {
+    return <p>Aucun outil utilisé</p>;
+  }
   return tools.map((tool) => (
     <Badge key={tool["@id"][tool["@id"].length - 1]}>{tool.name}</Badge>
   ));
