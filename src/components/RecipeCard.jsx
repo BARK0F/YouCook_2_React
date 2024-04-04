d9a159ad8470264eca392391e2c2e183e6daa0dc
Nassim MEKHALDI <nassim.mekhaldi@etudiant.univ-reims.fr>
2024-04-04 12:24:00 +0200

    rebase: utilisation du composant de Link + correction du lien vers la recette

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "wouter";
 import Badge from "./Badge";
 import Image from "./Image";
 
@@ -11,8 +12,8 @@ export default function RecipeCard({ recipe }) {
   sum /= recipe.marks.length;
   return (
     <div className="bg-white rounded-lg shadow-md p-4 max-w-sm m-4">
-      <a href={recipe.id}>
-        <Image source={"/img/recipe-default.png"}></Image>
+      <Link href={`recipes/${recipe.id}`}>
+        <Image source="/img/recipe-default.png" />
         <h5 className="text-lg font-bold mb-2">
           {recipe.name ?? "Nom de recette non existant"}
         </h5>
@@ -24,7 +25,7 @@ export default function RecipeCard({ recipe }) {
             Note : {!isNaN(sum) ? sum.toFixed(1) : "X.X"}
           </Badge>
         </div>
-      </a>
+      </Link>
     </div>
   );
 }
